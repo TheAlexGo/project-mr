5e3e1738ce66d4d8347a4b63b3a4db8f755fe9b3
Aleksandr Gordeev <alexander.gordeev@vk.team>
2023-04-04 23:59:58 +0300

    сжал изображения

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,14 +1,8 @@
 import ArchmageCover from './archmage.webp';
 import AyumuCover from './ayumu.webp';
-import BeginningCover from './beginning.webp';
-import BornCover from './born.webp';
-import FlameCover from './flame.webp';
 import KomiCover from './komi.webp';
-import LevelCover from './level.webp';
 import MagicCover from './magic.webp';
-import OmniscientCover from './omniscient.webp';
 import PeakCover from './peak.webp';
-import RankerCover from './ranker.webp';
 import SweetCover from './sweet.webp';
 import TrasheroCover from './trashero.webp';
 
@@ -16,13 +10,7 @@ export default [
     AyumuCover,
     KomiCover,
     MagicCover,
-    LevelCover,
-    BeginningCover,
-    FlameCover,
-    OmniscientCover,
     PeakCover,
-    RankerCover,
-    BornCover,
     SweetCover,
     TrasheroCover,
     ArchmageCover
